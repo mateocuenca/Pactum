8b4efc3c867d480bdec829ce7d4b26953d73646e
Mateo Cuenca <mateorc099@gmail.com>
2024-02-16 12:57:48 -0300

    fix: definition added for status code 200 step

--- features/support/steps.js
+++ features/support/steps.js
@@ -7,18 +7,25 @@ Before(() => {
     spec = pactum.spec();
 });
 
-
-Given(/^I make a (.*) request to (.*)$/, function (method, endpoint) {
-    spec[method.toLowerCase()](endpoint);
-});
+Given(/^I make a (.*) request to (.*)$/, (method, endpoint) =>
+    spec[method.toLowerCase()](endpoint));
 
 When('I receive a response', async function () {
     await spec.toss();
 });
 
-Then(/^I expect response should have a json like$/, function (json) {
-    spec.response().should.have.jsonLike(JSON.parse(json));
+
+Then(/^I expect response should have a status (.*)$/, (statusCode) =>
+    spec.response().should.have.status(Number(statusCode))
+);
+
+Then(/^I expect response should have a json at data$/, function (json) {
+    spec.response().should.have.json(JSON.parse(json));
 });
+
+Then(/^I expect response should have a json like$/, (json) =>
+    spec.response().should.have.jsonLike(JSON.parse(json))
+);
 
 After(() => {
     spec.end();
